71e15d5f2f470157fb563040237003762b6b61ad
Jo Walsh <369631+metazool@users.noreply.github.com>
2020-07-15 12:03:30 +0100

    Change the paths when we apply our theme

--- apply.sh
+++ apply.sh
@@ -1,12 +1,5 @@
 # replaces everything in vocprez/view/
 
 # replace styles
-rm -r /Users/nick/Work/surround/vocprez/vocprez/view/*
-cp -r /Users/nick/Work/surround/VocPrez-theme-generic/style ~/Work/surround/vocprez/vocprez/view/
-cp -r /Users/nick/Work/surround/VocPrez-theme-generic/templates ~/Work/surround/vocprez/vocprez/view/
-
-# configure config
-# cp ~/Work/surround/VocPrez-theme-generic/_config/__init__.py ~/Work/surround/vocprez/vocprez/_config/
-
-# purge routes
-cp ~/Work/surround/VocPrez-theme-generic/app.py ~/Work/surround/vocprez/vocprez/app.py
+cp -r ./VocPrez-theme-BGS/style ./VocPrez/vocprez/view/
+cp -r ./VocPrez-theme-BGS/templates ./VocPrez/vocprez/view/
